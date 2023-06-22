d9001e9a3dbdcf0de11187bf74c17c1603041c5c
Marcelo Abuchain <marceloea@gmail.com>
2023-06-22 12:20:38 -0300

    Primeira versao

--- IMC/src/abuchain/Principal.java
+++ IMC/src/abuchain/Principal.java
@@ -1,10 +1,22 @@
 package abuchain;
 
+import java.util.Scanner;
+
 public class Principal {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		int peso;
+		double altura;
+		double imc;
+		
+		Scanner teclado = new Scanner(System.in);
+		
+		peso = teclado.nextInt();
+		altura = teclado.nextDouble();
+		
+		imc = peso / (altura * altura);
+		
+		System.out.println(imc);
 	}
-
 }
