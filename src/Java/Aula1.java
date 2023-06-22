4bcc3e63b82c4830af761425d2c14b3759a47db4
Marcelo Abuchain <marceloea@gmail.com>
2023-06-22 00:16:29 -0300

    Teste Eclipse

--- src/Java/Aula1.java
+++ src/Java/Aula1.java
@@ -4,8 +4,9 @@ public class Aula1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int idade = 41;
+		int idade = 41-1;
 		System.out.println(idade);
+		
 	}
 
 }
